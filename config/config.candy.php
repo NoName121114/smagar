3f830e64df9ea294fbdb233bc611b3c8a2ab93e4
NoName121114 <92351544+NoName121114@users.noreply.github.com>
2023-02-28 12:47:50 +0700

    Create .csscomb.json

---
+++ config/config.candy.php
@@ -0,0 +1,9 @@
+<?php
+$copyright = 'SMAGAR JAYA Support by NoName1211';
+
+define("VERSI", "2.9.3");
+define("REVISI", "1 | NoName1211");
+define("APLIKASI", "SMAGAR");
+define("NAMA_DATABASE", "candy293");
+define("VERSI_DB", "2.9.3");
+define("BASEPATH", "cbtcandy");
